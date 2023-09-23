9accf9adc10b4ce2baaaaff2609e1ca19c44df23
Atyasha Bhattacharyya <139572321+atyasha2054@users.noreply.github.com>
2023-09-23 12:06:41 +0530

    Update final.py

--- final.py
+++ final.py
@@ -20,6 +20,11 @@ model = YOLO("yolov5m6u.pt")
 
 
 vid=cv2.VideoCapture("test_case4.jpg")
+height =int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+width =int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+fps=vid.get(cv2.CAP_PROP_FPS)
+out=cv2.VideoWriter(("mytest.mp4"),cv2.VideoWriter_fourcc('P','I','M','1'),fps,(width,height),isColor=False)
+out2=cv2.VideoWriter(("mytest_RGB.mp4"),cv2.VideoWriter_fourcc('P','I','M','1'),fps,(width,height),isColor=False)
 
 
 def mouse_events(event,x,y,flags,param):
@@ -32,10 +37,14 @@ def mouse_events(event,x,y,flags,param):
         x_mouse=x
         y_mouse=y
 
-
+D1={} # dictionary to add details
+num=0
+L2=[]
 
 while True:
-    
+    L1=[]   # list to collect the data of objects detected 
+    num=num+1
+    print(num)
     success,frame=vid.read()
     frame2= cv2.resize(frame, (960, 540))
 
@@ -44,12 +53,26 @@ while True:
     
     result=results[0]
     #cv2.imshow("frame",result.plot())
-    
+    for box in result.boxes:
+        label=result.names[box.cls[0].item()]
+        if label=="person":
+            L1.append(label)
+        #print("object",label)
+    no_of_per=len(L1)
+    if num%10!=0:
+        L2.append(no_of_per)
+    else:
+        
+        print(L2)
+        print(max(L2))
+        D1[num]=max(L2)
+        L2=[]
 
     # temperature part
     
     gray8=cv2.cvtColor(frame2, cv2.COLOR_RGB2GRAY)
-
+    gray82=cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    
     data=gray8[y_mouse,x_mouse]
     print(data)
 
@@ -58,3 +81,46 @@ while True:
 
     result_temp= cv2.applyColorMap(gray8, cv2.COLORMAP_JET)
     result_temp2= cv2.applyColorMap(gray8, cv2.COLORMAP_MAGMA)
+    screen1= np.concatenate((result_temp,frame2), axis=0)
+    screen2= np.concatenate((result.plot(),result_temp2), axis=0)
+    screen=np.concatenate((screen1,screen2),axis=1)
+
+cv2.imshow("out",screen)
+    out.write(gray82)
+    out2.write(frame)
+    
+    cv2.setMouseCallback("out",mouse_events)
+
+
+    if cv2.waitKey(10) & 0xFF==ord("q"):
+        break
+        
+cv2.destroyAllWindows()
+out.release()
+print(D1)
+
+
+#print("HI")
+key_lst=list(D1.keys())
+key_new=[]
+for i in key_lst:
+             i=str(i)
+             key_new.append(i)
+print(key_new)
+med=statistics.median(key_lst)
+print("median value of frames: ",med)
+for i in key_lst:
+    if i > med:
+        print(f"area {i} is high")
+    elif i<med:
+        print(f"area {i} is low")
+value_lst=list(D1.values())
+print(value_lst)
+plt.figure(figsize=(10,5))
+plt.bar(key_new,value_lst)
+plt.title('max person')
+plt.ylabel('person')
+plt.xlabel('no of frames')
+
+plt.show()
+plt.savefig("output.jpg")
